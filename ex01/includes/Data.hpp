fb8d17ab2d7eb4df97aa47624739495bbb9b2619
Eliam <Texsword76@gmail.com>
2025-04-30 13:35:15 +0200

    Add Serializer class for object serialization and deserialization
    Implemented the Serializer class to convert Data object pointers to raw uintptr_t values and back. This includes new header and source files for Serializer and a Data structure to demonstrate usage. This functionality is essential for safe and consistent data handling in pointer-based operations.

---
+++ ex01/includes/Data.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Eliam on 30/04/2025.
+//
+
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <string>
+
+struct Data {
+	int id;
+	std::string info;
+};
+
+#endif //DATA_HPP
---
+++ ex01/includes/Serializer.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Eliam on 30/04/2025.
+//
+
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include <cstdint>
+
+#include "Data.hpp"
+
+class Serializer {
+public:
+	static uintptr_t serialize(Data* ptr);
+	static Data* deserialize(uintptr_t raw);
+};
+
+#endif //SERIALIZER_HPP
